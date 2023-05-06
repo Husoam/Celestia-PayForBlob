f0160f30ad877304f2398dd609b7c7581ce86f9a
Husi <98665693+Husoam@users.noreply.github.com>
2023-05-06 14:02:07 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,30 +5,24 @@ export const Axios = axios.create({
     baseURL: 'http://localhost:26659/submit_pfb',
     withCredentials: true
 })
-//import { assertIsBroadcastTxSuccess, SigningStargateClient } from '@cosmjs/stargate'
 
 window.onload = async () => {
     // Keplr extension injects the offline signer that is compatible with cosmJS.
-    // You can get this offline signer from `window.getOfflineSigner(chainId:string)` after load event.
-    // And it also injects the helper function to `window.keplr`.
     // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
     if (!window.getOfflineSigner || !window.keplr) {
-        alert("Please install keplr extension");
+        alert("Please Install Keplr Extension");
     } else {
         if (window.keplr.experimentalSuggestChain) {
             try {
-                // Keplr v0.6.4 introduces an experimental feature that supports the feature to suggests the chain from a webpage.
-                // cosmoshub-3 is integrated to Keplr so the code should return without errors.
-                // The code below is not needed for cosmoshub-3, but may be helpful if you’re adding a custom chain.
                 // If the user approves, the chain will be added to the user's Keplr extension.
                 // If the user rejects it or the suggested chain information doesn't include the required fields, it will throw an error.
                 // If the same chain id is already registered, it will resolve and not require the user interactions.
                 await window.keplr.experimentalSuggestChain({
-                    // Chain-id of the Osmosis chain.
+                    // Chain-id of the Celestia chain.
                     chainId: "blockspacerace",
                     // The name of the chain to be displayed to the user.
                     chainName: "Blockspace Race Testnet",
-                    // RPC endpoint of the chain. In this case we are using blockapsis, as it's accepts connections from any host currently. No Cors limitations.
+                    // RPC endpoint of the chain.
                     rpc: "https://rpc-blockspacerace.pops.one",
                     // REST endpoint of the chain.
                     rest: "https://api-blockspacerace.pops.one",
@@ -40,29 +34,13 @@ window.onload = async () => {
                         coinMinimalDenom: "utia",
                         // # of decimal points to convert minimal denomination to user-facing denomination.
                         coinDecimals: 6,
-                        // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
                         // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
                         coinGeckoId: "celestia"
                     },
-                    // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
-                    // The 'stake' button in Keplr extension will link to the webpage.
-                    // walletUrlForStaking: "",
                     // The BIP44 path.
                     bip44: {
-                        // You can only set the coin type of BIP44.
-                        // 'Purpose' is fixed to 44.
                         coinType: 118,
                     },
-                    // Bech32 configuration to show the address to user.
-                    // This field is the interface of
-                    // {
-                    //   bech32PrefixAccAddr: string;
-                    //   bech32PrefixAccPub: string;
-                    //   bech32PrefixValAddr: string;
-                    //   bech32PrefixValPub: string;
-                    //   bech32PrefixConsAddr: string;
-                    //   bech32PrefixConsPub: string;
-                    // }
                     bech32Config: {
                         bech32PrefixAccAddr: "celestia",
                         bech32PrefixAccPub: "celestia" + "pub",
@@ -79,9 +57,8 @@ window.onload = async () => {
                         coinMinimalDenom: "utia",
                         // # of decimal points to convert minimal denomination to user-facing denomination.
                         coinDecimals: 6,
-                        // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
                         // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-                        // coinGeckoId: "celestia"
+                        coinGeckoId: "celestia"
                     }],
                     // List of coin/tokens used as a fee token in this chain.
                     feeCurrencies: [{
@@ -91,25 +68,14 @@ window.onload = async () => {
                         coinMinimalDenom: "utia",
                         // # of decimal points to convert minimal denomination to user-facing denomination.
                         coinDecimals: 6,
-                        // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
                         // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-                        // coinGeckoId: "celestia"
-                    }],
-                    // (Optional) The number of the coin type.
-                    // This field is only used to fetch the address from ENS.
-                    // Ideally, it is recommended to be the same with BIP44 path's coin type.
-                    // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
-                    // So, this is separated to support such chains.
-                    coinType: 118,
-                    // (Optional) This is used to set the fee of the transaction.
-                    // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
-                    // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
-                    // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
-                    gasPriceStep: {
+                        coinGeckoId: "celestia",
+			gasPriceStep: {
                         low: 0.01,
                         average: 0.025,
                         high: 0.04
-                    }
+                    	}
+                    }],
                 });
             } catch {
                 alert("Failed to suggest the chain");
@@ -124,7 +90,6 @@ window.onload = async () => {
     // You should request Keplr to enable the wallet.
     // This method will ask the user whether or not to allow access if they haven't visited this website.
     // Also, it will request user to unlock the wallet if the wallet is locked.
-    // If you don't request enabling before usage, there is no guarantee that other methods will work.
     await window.keplr.enable(chainId);
 
     const offlineSigner = window.getOfflineSigner(chainId);
@@ -135,30 +100,20 @@ window.onload = async () => {
     // XXX: This line is needed to set the sender address for SigningCosmosClient.
     const accounts = await offlineSigner.getAccounts();
 
-    // Initialize the gaia api with the offline signer that is injected by Keplr extension.
     const cosmJS = new SigningCosmosClient(
         "https://rpc-blockspacerace.pops.one",
         accounts[0].address,
         offlineSigner,
-    );
-
-  
+    ); 
 };
-
-//const namespace = document.getElementById("namespace");
-//const data = document.getElementById("data");
-//const submit = document.getElementById("save");
-
-
-
 	let loginForm = document.getElementById("loginForm");
-	
+	//get loginform
 	loginForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	
+	//listen submit button
 	let namespace = document.getElementById("namespace");
 	let data = document.getElementById("data");
-	 if (namespace.value == "" || data.value == "") {
+	if (namespace.value == "" || data.value == "") {
     alert("Ensure you input a value in both fields!");
   } else {
     // perform operation with form input
@@ -168,26 +123,26 @@ window.onload = async () => {
     );
   }
 	const postdata = {
-	  "namespace_id": namespace.value,
-      "data": data.value,
-	  "gas_limit": 80000, 
-	  "fee": 2000
+      	"namespace_id": namespace.value,
+      	"data": data.value,
+	"gas_limit": 80000, 
+	"fee": 2000
 	}
-
+	//Describe "Data" for curl post
 	const axios = require('axios');
-
-  axios.post('http://143.42.50.231:8010/proxy/submit_pfb', postdata ,
-  
-  {
-    headers: {
-      'Content-Type': 'text/plain'
-    }
+	axios.post('http://143.42.50.231:8010/proxy/submit_pfb', postdata , 
+	//Send post request with "axios" method on local node
+ {
+   	headers: {
+      	'Content-Type': 'text/plain'
+    	}
   }
 ).then((response) => { 
 	  const resp = response.data;
 	  const tx = (resp.txhash);
 	  const height = (resp.height);
 	  const txresp = ("TX: "+resp.txhash);
+	  //Parsing response for alert log
 	  alert(txresp);
 	  alert("Do you want to check via https://testnet.mintscan.io/ on explorer by click ok?")
 	  var txcheck = "https://testnet.mintscan.io/celestia-incentivized-testnet/txs/"+tx+"?height="+height
@@ -202,6 +157,7 @@ window.onload = async () => {
 		const err = error.response
 		const code = (err.status);
 		const msg = (err.statusText);
+		//Parsing response for error alarm log
 		console.log(error.response);
 		alert("Status Code: "+code+" "+msg+'\n'+"Please try again...");
 		location.reload();
@@ -212,7 +168,6 @@ window.onload = async () => {
 	//});
 	return false;
 });
-
 
 
         
